2825c3ece15cc6f48aac22b501515787b432efbc
Ahmadjon Dadabayev <98649872+Akhmadjonuz@users.noreply.github.com>
2022-06-01 15:30:15 +0500

    Update check.php

--- check.php
+++ check.php
@@ -1,12 +1,12 @@
 <?php
 if (isset($_GET['payment_status'])) {
     if ($_GET['payment_status'] <= 0) {
-       header('Location: https://example.com/noorder/click.php?message=Ошибка оплаты');
+       header('Location: https://example.com/click.php?message=Ошибка оплаты');
     }elseif ($_GET['payment_status'] == 1) {
-        header('Location: https://example.com/noorder/click.php?message=Платёж находится в обработке');
+        header('Location: https://example.com/click.php?message=Платёж находится в обработке');
     }elseif ($_GET['payment_status'] == 2 and isset($_GET['payment_id'])) {
-        header('Location: https://bots.uitc-host.uz/noorder/click.php?message=Платёж успешно проведён');
+        header('Location: https://example.com/click.php?message=Платёж успешно проведён');
     }else {
-        header('Location: https://bots.uitc-host.uz/noorder/click.php?message=Ошибка оплаты');
+        header('Location: https://example.com/click.php?message=Ошибка оплаты');
     }
 } 
